d2f2d8b903be9c03fd9636ca744ede98820f1c6e
Ilya Boltnev <roadtouniverse@gmail.com>
2017-09-21 15:13:42 +0300

    long recursive algorithm for finding all paths between words

--- strings/worladder.py
+++ strings/worladder.py
@@ -5,37 +5,52 @@ import enchant
 d = enchant.Dict('en')
 
 
-def find_word_ladder(source, dest, current_ladder = []):
+def find_word_ladder(source, dest, current_ladder = [], deadends = [], depth=0):
     if len(source) != len(dest):
         return None
 
     if source == dest:
         return current_ladder
+    if current_ladder == []:
+        current_ladder.append(source)
 
     ladders = []
 
     for i in range(len(source)):
-        if source[i] == dest[i]:
-            continue
+        for l in range(ord('a'), ord('z')+1):
+            l = chr(l)
+            if l == source[i]:
+                continue
 
-        word = source[:i]+dest[i]+source[i+1:]
+            word = source[:i]+l+source[i+1:]
+            if word == dest:
+                current_ladder.append(dest)
+                return [current_ladder]
 
-        if d.check(word):
-            ladders.append(current_ladder + [word])
+            if word in current_ladder or word in deadends:
+                continue
+
+            if d.check(word):
+                ladders.append(current_ladder + [word])
+
+    result = []
+    if ladders == []:
+        deadends.append(current_ladder[-1])
 
     for ladder in ladders:
-        print(ladder)
-        print(find_word_ladder(ladder[-1], dest, ladder))
-
-    return ladders
+        for l in find_word_ladder(ladder[-1], dest, ladder, deadends, depth+1):
+            if l[-1] == dest:
+                result.append(l)
+    print(result)
+    return result
 
 
 
 class TestWordLadder(unittest.TestCase):
 
     def test_word_ladder(self):
-        find_word_ladder('hit', 'cog')
-        pass
+        print(find_word_ladder('hit', 'cog'))
+        #find_word_ladder('test', 'cost')
 
 
 
